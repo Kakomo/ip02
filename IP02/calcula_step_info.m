cd5740279d761ba04d9bc45103709e78a0b47c59
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2023-01-13 08:04:37 -0300

    Update lab 2

--- IP02/calcula_step_info.m
+++ IP02/calcula_step_info.m
@@ -3,8 +3,8 @@ clear all
 %%
 load('identifica.mat')
 
-steadyState = mean(x(end-400+1:end,2))
+steadyState = mean(first_x(end-400+1:end,2))
 
-SInfo = stepinfo(x(:,2),x(:,1), steadyState);
+SInfo = stepinfo(first_x(:,2),first_x(:,1), steadyState);
 
 save('identifica.mat')
--- IP02/find_tf.m
+++ IP02/find_tf.m
@@ -9,13 +9,32 @@ tp = SInfo.PeakTime;
 ts = SInfo.SettlingTime;
 Mp = SInfo.Overshoot/100;
 
-omega = sqrt(( (pi^2)/tp^2) - 16/(ts^2))
+wn = sqrt(( (pi^2)/tp^2) - 16/(ts^2))
 zeta = 4/(omega * ts)
 
-Gmf = tf(omega^2, [1, 2*omega*zeta, omega^2])
+Gmf = tf(wn^2, [1, 2*wn*zeta, wn^2])
+C = 420;
 
-b = (omega^2)/420;
-a = -2*omega*zeta;
+b = (wn^2)/C;
+a = 2*wn*zeta;
 
-G = tf(b, [1, -a, 0])
+G = tf(b, [1, a, 0])
 
+%save('identifica.mat')
+
+wd = wn/sqrt(1-zeta^2);
+Td = 2*pi/wd;
+
+T =Td/8
+
+Gz = c2d(G, T)
+
+hold on
+step(feedback(G*C,1))
+step(feedback(Gz*C,1))
+
+T =Td/16
+
+Gz = c2d(G, T)
+step(feedback(Gz*C,1))
+hold off
