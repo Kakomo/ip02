7853c0ed4a9e5b7b0e0b95b52ebdd234e3d9f55b
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2023-01-18 17:51:10 -0300

    Add

--- IP02/find_tf.m
+++ IP02/find_tf.m
@@ -19,7 +19,7 @@ b = (wn^2)/C;
 a = 2*wn*zeta;
 
 G = tf(b, [1, a, 0])
-
+%%
 %save('identifica.mat')
 
 wd = wn/sqrt(1-zeta^2);
@@ -33,8 +33,24 @@ hold on
 step(feedback(G*C,1))
 step(feedback(Gz*C,1))
 
-T =Td/16
+T =Td/32
 
 Gz = c2d(G, T)
 step(feedback(Gz*C,1))
 hold off
+
+%% Especificações do sistema
+
+Mp_d = 0.15;
+ts_d = 0.3;
+
+zeta_d = sqrt(log(Mp_d)^2/(pi^2+log(Mp_d)^2))
+wn_d = 4/(zeta_d*ts_d)
+
+z1 = exp((-zeta_d*wn_d+i*wn_d)*T)
+z2 = exp((-zeta_d*wn_d-i*wn_d)*T)
+
+z_d = [z1 z2];
+
+z = tf('z',T);
+P = (z - z1)*(z - z2)
