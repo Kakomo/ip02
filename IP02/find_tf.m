682ab8fb6cff4f0ccd0bb0cfe9490c9d351b7c00
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2023-01-19 13:24:12 -0300

    PID implemented

--- IP02/find_tf.m
+++ IP02/find_tf.m
@@ -25,21 +25,22 @@ G = tf(b, [1, a, 0])
 wd = wn/sqrt(1-zeta^2);
 Td = 2*pi/wd;
 
-T =Td/8
+Ts =Td/8
 
-Gz = c2d(G, T)
+Gz = c2d(G, Ts)
 
 hold on
 step(feedback(G*C,1))
 step(feedback(Gz*C,1))
 
-T =Td/32
+Ts = 0.008 %aproximadamente Td/32
 
-Gz = c2d(G, T)
+Gz = c2d(G, Ts)
 step(feedback(Gz*C,1))
 hold off
 
 %% Especificações do sistema
+
 
 Mp_d = 0.15;
 ts_d = 0.3;
@@ -47,10 +48,16 @@ ts_d = 0.3;
 zeta_d = sqrt(log(Mp_d)^2/(pi^2+log(Mp_d)^2))
 wn_d = 4/(zeta_d*ts_d)
 
-z1 = exp((-zeta_d*wn_d+i*wn_d)*T)
-z2 = exp((-zeta_d*wn_d-i*wn_d)*T)
+z1 = exp((-zeta_d*wn_d+i*wn_d)*Ts)
+z2 = exp((-zeta_d*wn_d-i*wn_d)*Ts)
+
 
 z_d = [z1 z2];
 
-z = tf('z',T);
+z = tf('z',Ts);
 P = (z - z1)*(z - z2)
+
+% Claculado manualmente
+Kp = 0.245;
+Ki = 0.02;
+Kd = 0.734;
